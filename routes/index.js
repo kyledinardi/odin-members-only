682e2735e5efeeeb532442fb63dbb987c356eaac
kyledinardi <{ID}+{username}@users.noreply.github.com>
2024-06-03 12:37:21 -0400

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const userController = require('../controllers/userController');
+const messageController = require('../controllers/messageController');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', messageController.index);
+router.post('/new-message', messageController.newMessage);
+
+router.get('/sign-up', userController.signUpGet);
+router.post('/sign-up', userController.signUpPost);
+router.get('/login', userController.loginGet);
+router.post('/login', userController.loginPost);
+router.get('/join-club', userController.joinClubGet);
+router.post('/join-club', userController.joinClubPost);
+
 
 module.exports = router;
