8765c54837d89f62cdaeb1bf9c9cbc859a9492ff
kyledinardi <{ID}+{username}@users.noreply.github.com>
2024-06-03 12:20:17 -0400

    Add middleware and database connection

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
+require('dotenv').config();
 const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
+const debug = require('debug')('odin-members-only:server');
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const LocalStrategy = require('passport-local').Strategy;
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+app.use(helmet());
+
+const mongoDB = process.env.MONGODB_URI;
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main().catch((err) => debug(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +41,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
